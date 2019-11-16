898b4cfbb3498569d6cc49c20c2b8e7e9f99043b
Tomas Gear <tomas.e.gear@gmail.com>
2019-11-15 19:37:45 -0500

    added dotenv and password protection

---
+++ bamazonCustomer.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("dotenv").config(); 
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: process.env.MY_SQL_PASS,
+    database: "bamazon_db"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id: " + connection.threadId); 
+  });
