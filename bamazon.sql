6d5645521b0bade2ebc296e5f8d289d833b0990b
Tomas Gear <tomas.e.gear@gmail.com>
2019-11-15 19:12:43 -0500

    created products table and added 10x products

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL, 
+  price DECIMAL(10,2) NOT NULL, 
+  stock_quantity INTEGER NOT NULL, 
+  PRIMARY KEY (item_id)
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Dr. Bronner's Soap", "Beauty and Personal Care", 7.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Head and Shoulders Shampoo", "Beauty and Personal Care", 6.99, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Panasonic Lumix Camera", "Electronics", 349.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Sony 4K TV", "Electronics", 899.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Glad 13 Gallon Garbage Bags", "Household Supplies", 10.99, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("AA Batteries", "Household Supplies", 14.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Cascade Dishwasher Detergent", "Household Supplies", 7.99, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Kleenex Ultra Soft Tissues", "Beauty and Personal Care", 4.99, 60);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Nonstick 13-inch Skillet", "Kitchen", 28.99, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Wooden Spoon", "Kitchen", 2.99, 20);
